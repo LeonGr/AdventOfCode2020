fc199706d76f82ee0c8b20df199c1cc82bed203d
LeonGr <leongrasmeijer@gmail.com>
2020-12-12 13:13:44 +0100

    clean up day 12

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,6 +1,6 @@
-use std::{collections::HashSet, io::BufRead};
+use std::io::BufRead;
 
-use ndarray::{Array, arr2};
+use ndarray::{Array1, arr2};
 
 fn read_input_lines() -> std::io::Result<Vec<String>> {
     let input_file = std::fs::File::open("input")?;
@@ -12,41 +12,76 @@ fn read_input_lines() -> std::io::Result<Vec<String>> {
         .collect())
 }
 
-fn part1(lines: &Vec<String>) {
-    let mut position = Array::from(vec![0, 0]);
-    let mut direction = Array::from(vec![1, 0]);
+enum Part {
+    P1,
+    P2,
+}
+
+fn navigate(lines: &Vec<String>, part: Part) -> i32 {
+    let mut position = Array1::from(vec![0, 0]);
+    let mut direction = match part {
+        Part::P1 => Array1::from(vec![1, 0]),
+        Part::P2 => Array1::from(vec![10, 1]),
+    };
 
     for line in lines {
         let mut chars = line.chars();
         let action = chars.nth(0).unwrap();
         let mut value = chars.skip(0).collect::<String>().parse::<i32>().unwrap();
 
+        let mut delta: Option<Array1<i32>> = None;
+
         match action {
             'F' => position = position + value * &direction,
-            'N' => position = position + Array::from(vec![0, value]),
-            'E' => position = position + Array::from(vec![value, 0]),
-            'S' => position = position + Array::from(vec![0, -value]),
-            'W' => position = position + Array::from(vec![-value, 0]),
+            'N' => {
+                delta = Some(Array1::from(vec![0, value]));
+            },
+            'E' => {
+                delta = Some(Array1::from(vec![value, 0]));
+            },
+            'S' => {
+                delta = Some(Array1::from(vec![0, -value]));
+            },
+            'W' => {
+                delta = Some(Array1::from(vec![-value, 0]));
+            },
             'L' | 'R' => {
                 if action == 'R' {
                     value *= -1;
                 }
                 let angle = (value as f64).to_radians();
-                let ca = angle.cos() as i32;
-                let sa = angle.sin() as i32;
-                let rotation_matrix = arr2(&[[ca, -sa], [sa, ca]]);
+                let cos_a = angle.cos() as i32;
+                let sin_a = angle.sin() as i32;
+                let rotation_matrix = arr2(&[[cos_a, -sin_a], [sin_a, cos_a]]);
+
                 direction = rotation_matrix.dot(&direction);
             }
             _ => unreachable!(),
+        }
+
+        match delta {
+            Some(delta) => {
+                match part {
+                    Part::P1 => position = position + delta,
+                    Part::P2 => direction = direction + delta,
+                }
+            }
+            None => continue,
         }
     }
 
     let x = position.get(0).unwrap();
     let y = position.get(1).unwrap();
-    println!("{}", x.abs() + y.abs());
+
+    x.abs() + y.abs()
+}
+
+fn part1(lines: &Vec<String>) {
+    println!("{}", navigate(lines, Part::P1));
 }
 
 fn part2(lines: &Vec<String>) {
+    println!("{}", navigate(lines, Part::P2));
 }
 
 fn main() -> std::io::Result<()> {
