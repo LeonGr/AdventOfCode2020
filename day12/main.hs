34eb49bfcb6bdcd569aadfae9c18ad9e82e7e2dc
LeonGr <leongrasmeijer@gmail.com>
2020-12-12 17:05:32 +0100

    Add comments to Haskell implementation

--- day12/main.hs
+++ day12/main.hs
@@ -6,15 +6,22 @@ main = do
 
 type Vector = (Int, Int)
 
+-- Convert list of input lines to list of tuples (action, value)
 parseLines :: [String] -> [(Char, Int)]
 parseLines = map (\ x -> (head x, read (tail x) :: Int))
 
+-- Calculate manhattan distance between start point and reached pointed after following instructions
 navigatePart1 :: [(Char, Int)] -> Int
-navigatePart1 = rec (0, 0) (1, 0)
+navigatePart1 = rec (0, 0) (1, 0) -- call with starting position (0, 0), direction (1, 0) and instructions
     where
+        -- Recursive subfunction rec
+        -- Takes the current position, current direction and instructions left
         rec :: Vector -> Vector -> [(Char, Int)] -> Int
+        -- If we have no instructions left return the manhattan distance
         rec (x, y) _ [] = abs x + abs y
-        rec pos@(px, py) dir@(dx, dy) ((act, val):ins)
+        -- Otherwise, update current position or direction according to the first instruction (act, val)
+        -- and recurse with the new values and the remaining instructions
+        rec pos dir ((act, val):ins)
           | act == 'F' = rec (vectorAdd pos (scalarMul val dir )) dir                    ins
           | act == 'N' = rec (vectorAdd pos (0, val))             dir                    ins
           | act == 'E' = rec (vectorAdd pos (val, 0))             dir                    ins
@@ -24,12 +31,13 @@ navigatePart1 = rec (0, 0) (1, 0)
           | act == 'R' = rec pos                                  (rotateVec (-val) dir) ins
           | otherwise     = error "Unknown instruction"
 
+-- See part 1
 navigatePart2 :: [(Char, Int)] -> Int
 navigatePart2 = rec (0, 0) (10, 1)
     where
         rec :: Vector -> Vector -> [(Char, Int)] -> Int
         rec (x, y) _ [] = abs x + abs y
-        rec pos@(px, py) dir@(dx, dy) ((act, val):ins)
+        rec pos dir ((act, val):ins)
           | act == 'F' = rec (vectorAdd pos (scalarMul val dir)) dir                       ins
           | act == 'N' = rec pos                                 (vectorAdd dir (0, val))  ins
           | act == 'E' = rec pos                                 (vectorAdd dir (val, 0))  ins
@@ -38,6 +46,9 @@ navigatePart2 = rec (0, 0) (10, 1)
           | act == 'L' = rec pos                                 (rotateVec val dir)       ins
           | act == 'R' = rec pos                                 (rotateVec (-val) dir)    ins
           | otherwise     = error "Unknown instruction"
+
+
+-- Some quick math functions
 
 rotateVec :: Int -> Vector -> Vector
 rotateVec deg (x, y) = (round (xf * cos rad - yf * sin rad),
