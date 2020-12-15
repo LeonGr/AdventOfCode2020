c4b6918598f2cc953fd322b04c3fef195b43d56c
LeonGr <leongrasmeijer@gmail.com>
2020-12-15 16:28:47 +0100

    remove unnecessary code

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,18 +1,12 @@
-use std::collections::{HashMap, HashSet};
-
-#[derive(Debug)]
-struct Seen {
-    first: u32,
-    last: u32,
-}
+use std::collections::HashMap;
 
 fn run(input: &[u32], max_turn: u32) {
     let mut turn = 1;
 
-    let mut tracker: HashMap<u32, Seen> = HashMap::new();
+    let mut tracker: HashMap<u32, u32> = HashMap::new();
 
     for i in 0..input.len() {
-        tracker.insert(input[i], Seen { first: turn, last: turn });
+        tracker.insert(input[i], turn);
         turn += 1;
     }
 
@@ -21,18 +15,20 @@ fn run(input: &[u32], max_turn: u32) {
     while turn <= max_turn {
         match tracker.get(&last_spoken) {
             Some(last_seen_info) => {
-                let (first, last) = (last_seen_info.first, last_seen_info.last);
+                let last = last_seen_info.to_owned();
 
-                tracker.insert(last_spoken, Seen {first, last: turn - 1});
+                tracker.insert(last_spoken, turn - 1);
 
-                if turn - 1 == last {
-                    last_spoken = 0;
-                } else {
-                    last_spoken = turn - 1 - last;
+                last_spoken = {
+                    if turn - 1 == last {
+                        0
+                    } else {
+                        turn - 1 - last
+                    }
                 }
             }
             None => {
-                tracker.insert(last_spoken, Seen { first: turn - 1, last: turn - 1 });
+                tracker.insert(last_spoken, turn - 1);
                 last_spoken = 0;
             }
         }
@@ -44,7 +40,7 @@ fn run(input: &[u32], max_turn: u32) {
 }
 
 fn main() -> std::io::Result<()> {
-    let input = [18,11,9,0,5,1];
+    let input = [18, 11, 9, 0, 5, 1];
 
     run(&input, 2020u32);
     run(&input, 30000000u32);
