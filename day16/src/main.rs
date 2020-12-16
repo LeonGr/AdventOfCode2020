ade099ef13ac1c50c69857fe04e61dd85a35b59a
LeonGr <leongrasmeijer@gmail.com>
2020-12-16 14:50:05 +0100

    cleanup

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,6 +1,7 @@
-use regex::Regex;
-use std::{collections::{HashMap, HashSet}, io::BufRead};
 use std::ops::RangeInclusive;
+use std::{ collections::{HashMap, HashSet}, io::BufRead };
+
+type ValidRange = RangeInclusive<u16>;
 
 fn read_input_lines() -> std::io::Result<Vec<String>> {
     let input_file = std::fs::File::open("input")?;
@@ -12,23 +13,15 @@ fn read_input_lines() -> std::io::Result<Vec<String>> {
         .collect())
 }
 
-#[derive(Debug)]
-struct Ticket {
-    values: Vec<u16>,
-}
-
-#[derive(Debug)]
-struct Parsed {
-    fields: HashMap<String, (RangeInclusive<u16>, RangeInclusive<u16>)>,
-    my_ticket: Ticket,
-    nearby_tickets: Vec<Ticket>,
+fn parse_fields_string(line: &String) -> Vec<u16> {
+    line.split(",").map(|n| n.parse::<u16>().unwrap()).collect()
 }
 
 fn parse_input(lines: &Vec<String>) -> Parsed {
     let mut current_stage = 0;
 
-    let mut fields: HashMap<String, (RangeInclusive<u16>, RangeInclusive<u16>)> = HashMap::new();
-    let mut my_ticket_values: Vec<u16> = vec![];
+    let mut fields: HashMap<String, (ValidRange, ValidRange)> = HashMap::new();
+    let mut values: Vec<u16> = vec![];
     let mut nearby_tickets_values: Vec<Vec<u16>> = vec![];
 
     for line in lines {
@@ -46,63 +39,68 @@ fn parse_input(lines: &Vec<String>) -> Parsed {
             let ranges = parts[1]
                 .split(" or ")
                 .map(|range| {
-                    let numbers = range.split("-").map(|n| n.parse::<u16>().unwrap()).collect::<Vec<u16>>();
+                    let numbers = range
+                        .split("-")
+                        .map(|n| n.parse::<u16>().unwrap())
+                        .collect::<Vec<u16>>();
                     numbers[0]..=numbers[1]
                 })
-                .collect::<Vec<RangeInclusive<u16>>>();
+                .collect::<Vec<ValidRange>>();
 
-            fields.insert(key.to_string(), (ranges[0].to_owned(), ranges[1].to_owned()));
+            fields.insert(key.to_string(), (ranges[0].to_owned(), ranges[1].to_owned()),
+            );
         } else if current_stage == 1 {
-            my_ticket_values = line
-                .split(",")
-                .map(|n| n.parse::<u16>().unwrap())
-                .collect();
+            values = parse_fields_string(line);
         } else if current_stage == 2 {
-            nearby_tickets_values.push({
-                line
-                    .split(",")
-                    .map(|n| n.parse::<u16>().unwrap())
-                    .collect()
-            });
+            nearby_tickets_values.push(parse_fields_string(line));
         }
     }
 
-    let mut nearby_tickets: Vec<Ticket> = vec![];
+    let nearby_tickets: Vec<Ticket> =
+        nearby_tickets_values
+            .into_iter()
+            .fold(vec![], |mut acc, values| {
+                acc.push(Ticket { values });
+                acc
+            });
 
-    for nearby_ticket in nearby_tickets_values {
-        nearby_tickets.push(Ticket { values: nearby_ticket });
-    }
-
-    let my_ticket = Ticket { values: my_ticket_values };
+    let my_ticket = Ticket { values };
 
     return Parsed { fields, my_ticket, nearby_tickets };
 }
 
-//let amount_re = Regex::new(r"^\d+").unwrap();
-fn part1(parsed: &Parsed) {
-    let ticket_scanning_error_rate = parsed.nearby_tickets
-        .iter()
-        .fold(0, |acc, ticket| {
-            acc + ticket.values
-                .iter()
-                .fold(0, |total, value| {
-                    let valid_somewhere = parsed.fields.values()
-                        .any(|(range1, range2)| {
-                            range1.contains(&value) || range2.contains(&value)
-                        });
+#[derive(Debug)]
+struct Ticket {
+    values: Vec<u16>,
+}
 
-                    if !valid_somewhere {
-                        total + value
-                    } else {
-                        total
-                    }
-                })
-        });
+#[derive(Debug)]
+struct Parsed {
+    fields: HashMap<String, (ValidRange, ValidRange)>,
+    my_ticket: Ticket,
+    nearby_tickets: Vec<Ticket>,
+}
+
+fn part1(parsed: &Parsed) {
+    let ticket_scanning_error_rate = parsed.nearby_tickets.iter().fold(0, |acc, ticket| {
+        acc + ticket.values.iter().fold(0, |total, value| {
+            let valid_somewhere = parsed
+                .fields
+                .values()
+                .any(|(range1, range2)| range1.contains(&value) || range2.contains(&value));
+
+            if !valid_somewhere {
+                total + value
+            } else {
+                total
+            }
+        })
+    });
 
     println!("error rate {}", ticket_scanning_error_rate);
 }
 
-fn possible_fields(value: u16, fields: &HashMap<String, (RangeInclusive<u16>, RangeInclusive<u16>)>) -> HashSet<String> {
+fn possible_fields(value: u16, fields: &HashMap<String, (ValidRange, ValidRange)>) -> HashSet<String> {
     fields
         .iter()
         .fold(HashSet::new(), |mut acc, (field_name, (range1, range2))| {
@@ -118,20 +116,20 @@ fn part2(parsed: &Parsed) {
     let mut valid_tickets: Vec<&Ticket> = parsed.nearby_tickets
         .iter()
         .filter(|ticket| {
-            ticket.values
-                .iter()
-                .all(|value| {
-                    parsed.fields.values()
-                        .any(|(range1, range2)| {
-                            range1.contains(&value) || range2.contains(&value)
-                        })
-                })
+            ticket.values.iter().all(|value| {
+                parsed.fields
+                    .values()
+                    .any(|(range1, range2)| range1.contains(&value) || range2.contains(&value))
+            })
         })
         .collect();
 
     valid_tickets.push(&parsed.my_ticket);
 
-    let mut keys_to_find = parsed.fields.keys().map(|x| x.to_owned()).collect::<HashSet<String>>();
+    let mut keys_to_find = parsed.fields
+        .keys()
+        .map(|x| x.to_owned())
+        .collect::<HashSet<String>>();
     let mut ordered_fields = vec!["".to_string(); 20];
     let number_of_fields = keys_to_find.len();
     let mut fields_to_find = HashSet::new();
@@ -142,18 +140,20 @@ fn part2(parsed: &Parsed) {
 
     for _ in 0..number_of_fields {
         for i in fields_to_find.clone() {
-            let option = valid_tickets
+            let options = valid_tickets
                 .iter()
                 .fold(keys_to_find.clone(), |acc, ticket| {
-                    acc.intersection(&possible_fields(ticket.values[i], &parsed.fields)).map(|c| c.to_owned()).collect()
+                    acc.intersection(&possible_fields(ticket.values[i], &parsed.fields))
+                        .map(|x| x.to_owned())
+                        .collect()
                 })
                 .iter()
                 .map(|x| x.to_owned())
                 .collect::<Vec<String>>();
 
-            if option.len() == 1 {
-                keys_to_find.remove(&option[0]);
-                ordered_fields[i] = option[0].clone();
+            if options.len() == 1 {
+                keys_to_find.remove(&options[0]);
+                ordered_fields[i] = options[0].clone();
                 fields_to_find.remove(&i);
                 break;
             }
